a0a6775d8696467106f722dcf9c3fcac92cce23f
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2024-09-30 20:58:41 -0500

    Fix image cropping

--- flutter_app/lib/image_croper.dart
+++ flutter_app/lib/image_croper.dart
@@ -3,23 +3,24 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:heritage/profile_display.dart';
+import 'package:heritage/util.dart';
 import 'package:vector_math/vector_math_64.dart' hide Colors;
 
 Future<Uint8List?> showCropDialog(
-    BuildContext context, Uint8List image, Size size) async {
+    BuildContext context, Uint8List image, Size imageSize) async {
   final rect = await showDialog<Rect>(
     context: context,
     builder: (context) {
       return _CropDialog(
         image: image,
-        size: size,
+        size: imageSize,
       );
     },
   );
   if (!context.mounted || rect == null) {
     return null;
   }
-  return _cropImage(image, Offset.zero & size);
+  return _cropImage(image, rect);
 }
 
 class _CropDialog extends StatefulWidget {
@@ -47,18 +48,18 @@ class _CropDialogState extends State<_CropDialog> {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          ImageCropper(
-            image: widget.image,
-            size: widget.size,
-            onRect: (rect) {
-              setState(() => _rect = rect);
-            },
+          Flexible(
+            child: ImageCropper(
+              image: widget.image,
+              imageSize: widget.size,
+              onRect: (rect) => setState(() => _rect = rect),
+            ),
           ),
           const SizedBox(height: 16),
           FilledButton(
             onPressed: () => Navigator.of(context).pop(_rect),
             style: FilledButton.styleFrom(
-              fixedSize: const Size.fromHeight(73),
+              fixedSize: const Size.fromHeight(50),
             ),
             child: const Text('Done'),
           ),
@@ -70,13 +71,13 @@ class _CropDialogState extends State<_CropDialog> {
 
 class ImageCropper extends StatefulWidget {
   final Uint8List image;
-  final Size size;
+  final Size imageSize;
   final void Function(Rect rect) onRect;
 
   const ImageCropper({
     super.key,
     required this.image,
-    required this.size,
+    required this.imageSize,
     required this.onRect,
   });
 
@@ -85,36 +86,95 @@ class ImageCropper extends StatefulWidget {
 }
 
 class _ImageCropperState extends State<ImageCropper> {
-  late final TransformationController _controller;
+  final _controller = TransformationController();
+  final _interactiveViewerKey = GlobalKey();
+  bool _hasSize = false;
+  Size _viewportSize = Size.zero;
+  Size _contentSize = Size.zero;
 
   @override
   void initState() {
     super.initState();
-    const outputSize = Size(300, 400);
-    final fittedSizes = applyBoxFit(BoxFit.cover, widget.size, outputSize);
-    final scale = fittedSizes.destination.height / fittedSizes.source.height;
-    final translation = fittedSizes.source.width / widget.size.width;
-    final outputRect =
-        Alignment.center.inscribe(fittedSizes.source, Offset.zero & outputSize);
-    final t = outputRect.topLeft;
-    final s = outputSize.height / outputRect.height;
-    final matrix = Matrix4.identity()
-      ..translate(t.dx, 0.0, 0.0)
-      // ..translate(widget.size.width / 2, widget.size.height / 2, 0.0)
-      ..scale(s);
-    // ..translate(-widget.size.width / 2, -widget.size.height / 2, 0.0);
+    WidgetsBinding.instance.endOfFrame.then((_) {
+      if (mounted) {
+        _computeSizes();
+        _controller.addListener(_onTransform);
 
-    _controller = TransformationController(matrix);
-    _controller.addListener(_onTransform);
+        // Initial value
+        _onTransform();
+      }
+    });
+  }
+
+  void _computeSizes() {
+    final viewportSize = locateWidget(_interactiveViewerKey)?.size ?? Size.zero;
+
+    // Content scaled to cover viewport, one dimension is larger than FittedSizes.destination
+    final imageAspectRatio = widget.imageSize.width / widget.imageSize.height;
+    final viewportAspectRatio = viewportSize.width / viewportSize.height;
+    final double scale;
+    if (imageAspectRatio > viewportAspectRatio) {
+      scale = viewportSize.height / widget.imageSize.height;
+    } else {
+      scale = viewportSize.width / widget.imageSize.width;
+    }
+    final contentSize = widget.imageSize * scale;
+    final viewportRect =
+        Alignment.center.inscribe(contentSize, Offset.zero & viewportSize);
+
+    setState(() {
+      _contentSize = contentSize;
+      _viewportSize = viewportSize;
+      _hasSize = true;
+      _controller.value = Matrix4.identity()
+        ..translate(viewportRect.left, viewportRect.top, 0.0);
+    });
   }
 
   void _onTransform() {
     final matrix = _controller.value;
-    final scale = matrix.getMaxScaleOnAxis();
-    final Vector3 tl = -(matrix * Vector3.zero()) / scale;
-    final Vector3 br = Vector3.zero();
-    final rect = Rect.fromLTWH(tl.x, tl.y, br.x - tl.x, br.y - tl.y);
+    final rect =
+        getCropRect(matrix, _contentSize, widget.imageSize, _viewportSize);
     widget.onRect(rect);
+  }
+
+  void _onSliderScale(double scale) {
+    // Relative zoom
+    final matrix = _controller.value.clone();
+    final scaleFactor = scale / matrix.getMaxScaleOnAxis();
+    final scalePivot = _viewportSize / 2;
+    final newMatrix = Matrix4.identity()
+      ..translate(scalePivot.width, scalePivot.height)
+      ..scale(scaleFactor)
+      ..translate(-scalePivot.width, -scalePivot.height)
+      ..multiply(matrix);
+
+    // Stops going out of bounds when zooming out
+    final scaledContent = _contentSize * scale;
+    final t = newMatrix.getTranslation();
+    double tx = t.x;
+    double ty = t.y;
+    tx = tx.clamp(_viewportSize.width - scaledContent.width, 0);
+    ty = ty.clamp(_viewportSize.height - scaledContent.height, 0);
+    newMatrix.setTranslationRaw(tx, ty, 0.0);
+
+    _controller.value = newMatrix;
+  }
+
+  Rect getCropRect(Matrix4 matrix, Size scaledImageSize, Size originalImageSize,
+      Size viewportSize) {
+    final m = Matrix4.inverted(matrix);
+    final topLeft = m.transform3(Vector3.zero());
+    final bottomRight =
+        m.transform3(Vector3(viewportSize.width, viewportSize.height, 0));
+
+    final scale = originalImageSize.width / scaledImageSize.width;
+    return Rect.fromLTRB(
+      topLeft.x * scale,
+      topLeft.y * scale,
+      bottomRight.x * scale,
+      bottomRight.y * scale,
+    );
   }
 
   @override
@@ -123,85 +183,67 @@ class _ImageCropperState extends State<ImageCropper> {
     super.dispose();
   }
 
-  Vector3 getTopLeftInChildCoordinates(Matrix4 matrix, Vector3 childSize) {
-    // Extract the translation (shift) from the matrix
-    final translation = matrix.getTranslation();
-
-    // Extract the scale from the matrix (assuming uniform scaling on all axes)
-    final scale = matrix.getMaxScaleOnAxis();
-
-    // The translation represents the shift of the content.
-    // Since we need to map it back to the child coordinates,
-    // we divide by the scale to reverse the zoom effect.
-    final topLeftX = -translation.x / scale;
-    final topLeftY = -translation.y / scale;
-
-    return Vector3(topLeftX, topLeftY, 0.0);
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        SizedBox(
-          width: 400,
-          child: ImageAspect(
-            child: _BorderOverlay(
-              child: MouseRegion(
-                cursor: SystemMouseCursors.move,
-                child: InteractiveViewer(
-                  clipBehavior: Clip.none,
-                  transformationController: _controller,
-                  constrained: false,
-
-                  // alignment: Alignment.center,
-                  minScale: 0.1,
-                  maxScale: 3.5,
-                  child: SizedBox(
-                    height: 400,
-                    width: 400,
-                    child: Image.memory(
-                      widget.image,
+    return Opacity(
+      opacity: _hasSize ? 1.0 : 0.0,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Flexible(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 600, maxHeight: 800),
+              child: ImageAspect(
+                child: _BorderOverlay(
+                  child: MouseRegion(
+                    cursor: SystemMouseCursors.move,
+                    child: InteractiveViewer(
+                      key: _interactiveViewerKey,
+                      clipBehavior: Clip.none,
+                      transformationController: _controller,
+                      constrained: false,
+                      minScale: 1,
+                      maxScale: 3.5,
+                      child: SizedBox(
+                        width: _contentSize.width,
+                        height: _contentSize.height,
+                        child: Image.memory(
+                          widget.image,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
-        const SizedBox(height: 16),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 32.0),
-          child: Row(
-            children: [
-              const Icon(Icons.zoom_out),
-              Expanded(
-                child: AnimatedBuilder(
-                  animation: _controller,
-                  builder: (context, child) {
-                    return Slider(
-                      min: 0.1,
-                      max: 3.5,
-                      value:
-                          _controller.value.getMaxScaleOnAxis().clamp(0.1, 3.5),
-                      onChanged: (scale) {
-                        final centerX = widget.size.width / 2;
-                        final centerY = widget.size.height / 2;
-                        _controller.value = Matrix4.identity()
-                          ..translate(centerX, centerY, 0.0)
-                          ..scale(scale)
-                          ..translate(-centerX, -centerY, 0.0);
-                      },
-                    );
-                  },
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: Row(
+              children: [
+                const Icon(Icons.zoom_out),
+                Expanded(
+                  child: AnimatedBuilder(
+                    animation: _controller,
+                    builder: (context, child) {
+                      return Slider(
+                        min: 1,
+                        max: 3.5,
+                        value:
+                            _controller.value.getMaxScaleOnAxis().clamp(1, 3.5),
+                        onChanged: _onSliderScale,
+                      );
+                    },
+                  ),
                 ),
-              ),
-              const Icon(Icons.zoom_in),
-            ],
+                const Icon(Icons.zoom_in),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
@@ -215,7 +257,7 @@ class _BorderOverlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const borderColor = Color.fromRGBO(0xFF, 0xFF, 0xFF, 0.6);
+    const borderColor = Color.fromRGBO(0xFF, 0xFF, 0xFF, 0.5);
     const borderSize = 48.0;
     return ClipRect(
       child: Stack(
@@ -293,12 +335,13 @@ Future<Uint8List?> _cropImage(Uint8List imageBytes, Rect rect) async {
   final canvas = Canvas(recorder);
   canvas.drawImageRect(
     image,
-    Offset.zero & Size(image.width.toDouble(), image.height.toDouble()),
     rect,
+    Offset.zero & rect.size,
     Paint(),
   );
   final picture = recorder.endRecording();
-  final cropped = await picture.toImage(image.width, image.height);
+  final cropped =
+      await picture.toImage(rect.size.width.toInt(), rect.size.height.toInt());
   final byteData = await cropped.toByteData(format: ImageByteFormat.png);
   image.dispose();
   return byteData?.buffer.asUint8List();
