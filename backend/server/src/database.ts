4b136734ecdfeaff593a938d68c33845c7acda1b
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2024-10-07 18:43:32 -0500

    Increase max distance of fetched tree

--- backend/server/src/database.ts
+++ backend/server/src/database.ts
@@ -140,7 +140,7 @@ export class Database {
   }
 
   public async getLimitedGraph(id: Id): Promise<Person[]> {
-    return this.fetchUpToDistance(id, { maxDistance: 3, traverseSilbingsChildrenAfterRelativeLevel: -2 });
+    return this.fetchUpToDistance(id, { maxDistance: 6, traverseSilbingsChildrenAfterRelativeLevel: -2 });
   }
 
   public async getPerson(id: Id): Promise<Person> {
