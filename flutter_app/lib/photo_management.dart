1093c5d0ab04f1676ad1b3ca589048ec8fc53463
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2024-10-07 20:01:14 -0500

    Adjust photo management appearance

--- flutter_app/lib/photo_management.dart
+++ flutter_app/lib/photo_management.dart
@@ -3,11 +3,11 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:heritage/api.dart';
-import 'package:heritage/profile_display.dart';
 import 'package:heritage/util.dart';
 import 'package:http/http.dart';
+import 'package:path_drawing/path_drawing.dart' as path_drawing;
 
-const height = 80.0;
+const height = 71.0;
 
 class PhotoManagement extends StatefulWidget {
   final UnmodifiableListView<Photo> gallery;
@@ -71,86 +71,96 @@ class _PhotoManagementState extends State<PhotoManagement> {
             itemBuilder: (context, index) {
               final photo = widget.gallery[index];
               final key = _keys[index];
-              return MouseRegion(
+              return Padding(
                 key: Key('$index'),
-                cursor: SystemMouseCursors.move,
-                child: ReorderableDragStartListener(
-                  index: index,
-                  child: _Thumbnail(
-                    key: key,
-                    photo: photo,
-                    onTap: () async {
-                      final rect = locateWidget(key) ?? Rect.zero;
-                      showMenu(
-                        context: context,
-                        position: RelativeRect.fromLTRB(
-                          rect.left,
-                          rect.top,
-                          rect.right,
-                          rect.bottom,
-                        ).shift(const Offset(10, height)),
-                        items: [
-                          PopupMenuItem(
-                            onTap: () async {
-                              final imageFuture = photo.map(
-                                network: (network) => _download(network.url),
-                                memory: (memory) => Future.value(memory.bytes),
-                              );
-                              final image =
-                                  await showBlockingModal(context, imageFuture);
-                              if (image == null || !context.mounted) {
-                                return;
-                              }
-                              final cropped = await showCropperForImage(
-                                context,
-                                image: image,
-                                faceMask: true,
-                              );
-                              if (cropped == null || !context.mounted) {
-                                return;
-                              }
-                              widget.onProfilePhotoChanged(cropped);
-                            },
-                            child: const Text('Use as profile photo'),
+                padding: const EdgeInsets.only(right: 12.0),
+                child: MouseRegion(
+                  cursor: SystemMouseCursors.move,
+                  child: ReorderableDragStartListener(
+                    index: index,
+                    child: _Thumbnail(
+                      key: key,
+                      photo: photo,
+                      onTap: () async {
+                        final rect = locateWidget(key) ?? Rect.zero;
+                        showMenu(
+                          context: context,
+                          popUpAnimationStyle: AnimationStyle.noAnimation,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
                           ),
-                          PopupMenuItem(
-                            onTap: () async {
-                              final delete = await showDialog(
-                                context: context,
-                                builder: (context) {
-                                  return AlertDialog(
-                                    title: const Text('Delete photo?'),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: Navigator.of(context).pop,
-                                        style: TextButton.styleFrom(
-                                          foregroundColor: Colors.black,
+                          position: RelativeRect.fromLTRB(
+                            rect.left,
+                            rect.top,
+                            rect.right,
+                            rect.bottom,
+                          ).shift(const Offset(10, height)),
+                          items: [
+                            PopupMenuItem(
+                              onTap: () async {
+                                final imageFuture = photo.map(
+                                  network: (network) => _download(network.url),
+                                  memory: (memory) =>
+                                      Future.value(memory.bytes),
+                                );
+                                final image = await showBlockingModal(
+                                    context, imageFuture);
+                                if (image == null || !context.mounted) {
+                                  return;
+                                }
+                                final cropped = await showCropperForImage(
+                                  context,
+                                  image: image,
+                                  faceMask: true,
+                                );
+                                if (cropped == null || !context.mounted) {
+                                  return;
+                                }
+                                widget.onProfilePhotoChanged(cropped);
+                              },
+                              child: const Text('Use as profile photo'),
+                            ),
+                            PopupMenuItem(
+                              onTap: () async {
+                                final delete = await showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      title: const Text('Delete photo?'),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: Navigator.of(context).pop,
+                                          style: TextButton.styleFrom(
+                                            foregroundColor: Colors.black,
+                                          ),
+                                          child: const Text('Cancel'),
                                         ),
-                                        child: const Text('Cancel'),
-                                      ),
-                                      TextButton(
-                                        onPressed: () =>
-                                            Navigator.of(context).pop(true),
-                                        style: TextButton.styleFrom(
-                                          foregroundColor: Colors.red,
+                                        TextButton(
+                                          onPressed: () =>
+                                              Navigator.of(context).pop(true),
+                                          style: TextButton.styleFrom(
+                                            foregroundColor: Colors.red,
+                                          ),
+                                          child: const Text('Delete'),
                                         ),
-                                        child: const Text('Delete'),
-                                      ),
-                                    ],
-                                  );
-                                },
-                              );
-                              if (delete == true && context.mounted) {
-                                final newGallery = List.of(widget.gallery);
-                                newGallery.removeAt(index);
-                                widget.onChanged(newGallery);
-                              }
-                            },
-                            child: const Text('Delete'),
-                          ),
-                        ],
-                      );
-                    },
+                                      ],
+                                    );
+                                  },
+                                );
+                                if (delete == true && context.mounted) {
+                                  final newGallery = List.of(widget.gallery);
+                                  newGallery.removeAt(index);
+                                  widget.onChanged(newGallery);
+                                }
+                              },
+                              child: const Text('Delete'),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
                   ),
                 ),
               );
@@ -189,17 +199,26 @@ class _AddImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _Container(
-      child: FilledButton(
-        onPressed: onPressed,
-        style: FilledButton.styleFrom(
-          shape: const RoundedRectangleBorder(),
-          backgroundColor: Colors.grey,
-        ),
-        child: const Center(
-          child: Icon(
-            Icons.add_photo_alternate,
-            color: Colors.white,
+    const color = Color.fromRGBO(0xB7, 0xB7, 0xB7, 1.0);
+    return _DashedBorder(
+      color: color,
+      radius: const Radius.circular(12),
+      child: _Container(
+        child: FilledButton(
+          onPressed: onPressed,
+          style: FilledButton.styleFrom(
+            shape: const RoundedRectangleBorder(),
+            backgroundColor: Colors.transparent,
+          ),
+          child: Transform.scale(
+            scaleX: -1,
+            child: const Center(
+              child: Icon(
+                Icons.add_a_photo,
+                color: Color.fromRGBO(0xB7, 0xB7, 0xB7, 1.0),
+                size: 30,
+              ),
+            ),
           ),
         ),
       ),
@@ -220,22 +239,29 @@ class _Thumbnail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return _Container(
-      child: FilledButton(
-        onPressed: onTap,
-        style: FilledButton.styleFrom(
-          shape: const RoundedRectangleBorder(),
+      child: FittedBox(
+        fit: BoxFit.cover,
+        child: FilledButton(
+          onPressed: onTap,
+          style: FilledButton.styleFrom(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(12),
+              ),
+            ),
+          ),
+          child: switch (photo) {
+            NetworkPhoto(:final url) => Image.network(
+                url,
+                fit: BoxFit.cover,
+              ),
+            MemoryPhoto(:final Uint8List bytes) => Image.memory(
+                bytes,
+                fit: BoxFit.cover,
+              ),
+            _ => const SizedBox.shrink(),
+          },
         ),
-        child: switch (photo) {
-          NetworkPhoto(:final url) => Image.network(
-              url,
-              fit: BoxFit.cover,
-            ),
-          MemoryPhoto(:final Uint8List bytes) => Image.memory(
-              bytes,
-              fit: BoxFit.cover,
-            ),
-          _ => const SizedBox.shrink(),
-        },
       ),
     );
   }
@@ -251,41 +277,86 @@ class _Container extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: SizedBox(
-        height: height,
-        child: ImageAspect(
-          child: Container(
-            clipBehavior: Clip.antiAlias,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(8),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 12,
-                  offset: Offset(0, 4),
-                  color: Color.fromRGBO(0x00, 0x00, 0x00, 0.25),
-                ),
-              ],
+    return SizedBox(
+      height: height,
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          clipBehavior: Clip.antiAlias,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(12),
             ),
-            child: child,
           ),
+          child: child,
         ),
       ),
     );
   }
 }
 
+class _DashedBorder extends StatelessWidget {
+  final Color color;
+  final Radius radius;
+  final Widget child;
+
+  const _DashedBorder({
+    super.key,
+    required this.color,
+    required this.radius,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      foregroundPainter: _DashedPainter(
+        color: color,
+        radius: radius,
+      ),
+      child: child,
+    );
+  }
+}
+
+class _DashedPainter extends CustomPainter {
+  final Color color;
+  final Radius radius;
+
+  _DashedPainter({
+    required this.color,
+    required this.radius,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final path = Path()
+      ..addRRect(RRect.fromRectAndRadius(Offset.zero & size, radius));
+    final dashedPath = path_drawing.dashPath(
+      path,
+      dashArray: path_drawing.CircularIntervalList<double>([4.0, 6.0]),
+    );
+    canvas.drawPath(
+      dashedPath,
+      Paint()
+        ..strokeWidth = 3
+        ..style = PaintingStyle.stroke
+        ..strokeCap = StrokeCap.round
+        ..color = color,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant _DashedPainter oldDelegate) {
+    return oldDelegate.color != color || oldDelegate.radius != radius;
+  }
+}
+
 Future<Uint8List?> _download(String url) async {
   try {
-    print('download');
     final response = await get(Uri.parse(url));
-    print('response ${response.contentLength}');
     return response.bodyBytes;
   } catch (e) {
-    print(e);
     return null;
   }
 }
