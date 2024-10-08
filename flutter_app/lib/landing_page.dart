9376601f6c3092da6595af1ae7b25763b7763c78
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2024-10-07 23:16:00 -0500

    Implement landing page design

--- flutter_app/lib/landing_page.dart
+++ flutter_app/lib/landing_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:heritage/family_tree_page_panels.dart';
 import 'package:heritage/storage.dart';
+import 'package:lottie/lottie.dart';
 
 class LandingPage extends StatefulWidget {
   final Storage storage;
@@ -27,10 +29,108 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(223, 235, 250, 1.0),
       body: Center(
-        child: widget.status == LandingPageStatus.invalidLink
-            ? const Text('Invalid link')
-            : const Text('This app is invite only!'),
+        child: Container(
+          width: 287,
+          height: 287,
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(
+              Radius.circular(25),
+            ),
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 3),
+                blurRadius: 20,
+                color: Color.fromRGBO(0x00, 0x00, 0x00, 0.2),
+              )
+            ],
+          ),
+          child: switch (widget.status) {
+            null || LandingPageStatus.decline => const _LandingPageContent(),
+            LandingPageStatus.invalidLink => const _InvalidLinkContent(),
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _LandingPageContent extends StatelessWidget {
+  const _LandingPageContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 24),
+        Lottie.asset(
+          'assets/images/logo.json',
+          width: 86,
+          height: 86,
+        ),
+        const SizedBox(height: 16),
+        const LogoText(
+          width: 230,
+        ),
+        const Spacer(),
+        const _InviteOnlyText(),
+        const SizedBox(height: 24),
+      ],
+    );
+  }
+}
+
+class _InvalidLinkContent extends StatelessWidget {
+  const _InvalidLinkContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        SizedBox(height: 24),
+        LogoText(
+          width: 230,
+        ),
+        SizedBox(height: 40),
+        Text(
+          'Invalid Link',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w700,
+            color: Color.fromRGBO(0x00, 0xAE, 0xFF, 1.0),
+          ),
+        ),
+        SizedBox(height: 4),
+        Text(
+          'Ask your family to resend a link!',
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            color: Color.fromRGBO(0x31, 0x31, 0x31, 1.0),
+          ),
+        ),
+        Spacer(),
+        _InviteOnlyText(),
+        SizedBox(height: 24),
+      ],
+    );
+  }
+}
+
+class _InviteOnlyText extends StatelessWidget {
+  const _InviteOnlyText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Stitchfam is currently invite only',
+      style: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: const Color.fromRGBO(0x9E, 0x9E, 0x9E, 1.0),
       ),
     );
   }
